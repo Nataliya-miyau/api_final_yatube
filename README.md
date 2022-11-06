cdf298007ba88407a9af8d10a8945758258870fa
Наталия Громова <mak_rel@mail.ru>
2022-11-06 17:27:23 +0300

    предварительная версия

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# api_final
-api final
+Учебный проект курса Python-разработчик (бекенд) от Яндекс.Практикума.
+API для Yatube
+(спринт 9)
+Описание
+API для Yatub  - это проект социальной сети, в которой авторизованный пользователь может публиковать и комментировать записи, а так же подписываться/отписываться на других авторов. Неавторизованному пользователю контент доступен только в режиме чтения.
+
+Технологии
+Python 3.7, Django, DRF, JWT + Djoser
+
+Установка:
+Клонируйте репозиторий и перейдите в него в командной строке:
+git clone <ссылка на репозиторий>
+cd api_final_yatube
+Установите и активируйте виртуальное окружение c учетом версии Python 3.7:
+python3.7 -m venv venv
+source venv/bin/activate
+python3.7 -m pip install --upgrade pip
+Установите все зависимости из файла requirements.txt:
+pip install -r requirements.txt
+Выполните миграции:
+python3.7 manage.py migrate
+Далее создадим суперпользователя:
+python manage.py createsuperuser
+Запустите проект:
+python3.7 manage.py runserver
+
+Примеры работы с API:
+
+- создание публикации
+
+POST /api/v1/posts/
+в body { "text": "string", "image": "string", "group": 0 }
+
+- обновление публикации
+
+PUT /api/v1/posts/{id}/
+в body { "text": "string", "image": "string", "group": 0 }
+
+PATCH /api/v1/posts/{id}/
+в body { "text": "string", "image": "string", "group": 0 }
+
+- удаление публикации
+
+DEL /api/v1/posts/{id}/
+
+- подписка пользователя, от имени которого сделан запрос
+  на пользователя, переданного в теле запроса
+
+GET /api/v1/follow/ 
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,66 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = ('id', 'text', 'author', 'group', 'pub_date')
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    post = serializers.PrimaryKeyRelatedField(
+        read_only=True
     )
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = ('id', 'text', 'author', 'post', 'created')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('title', 'slug', 'description')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+        validators = (
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message=('Подписка уже существует!')
+            ),
+        )
+
+    def validate(self, data):
+        if data['user'] == data['following']:
+            raise serializers.ValidationError(
+                'Нельзя подписаться на себя!')
+        return data
